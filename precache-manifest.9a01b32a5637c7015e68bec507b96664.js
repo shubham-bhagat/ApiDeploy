dd3ac018c3dc18d415fabfaaf7487b26d9da26b3
shubham-bhagat <38393906+shubham-bhagat@users.noreply.github.com>
2020-08-21 08:59:37 +0530

    Updates

---
+++ precache-manifest.9a01b32a5637c7015e68bec507b96664.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f1edbdcb9d5c156f71ae9cb174001e",
+    "url": "/ApiDeploy/index.html"
+  },
+  {
+    "revision": "977ba5a72e76639cdcac",
+    "url": "/ApiDeploy/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "0e9e36b4aae7f9e95dea",
+    "url": "/ApiDeploy/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "977ba5a72e76639cdcac",
+    "url": "/ApiDeploy/static/js/2.ae456e4c.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/ApiDeploy/static/js/2.ae456e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9e36b4aae7f9e95dea",
+    "url": "/ApiDeploy/static/js/main.98eacf4c.chunk.js"
+  },
+  {
+    "revision": "26afa470b4fb69af2c27",
+    "url": "/ApiDeploy/static/js/runtime-main.19935698.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ApiDeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
